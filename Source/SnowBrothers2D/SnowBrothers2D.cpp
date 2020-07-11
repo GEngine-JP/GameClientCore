d1802f34e9da53ecf2ec69a8b7c1bbaa4367b20e
xiaomo <xiaomo@xiaomo.info>
2020-07-11 10:26:00 +0800

    initial commit

---
+++ Source/SnowBrothers2D/SnowBrothers2D.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SnowBrothers2D.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnowBrothers2D, "SnowBrothers2D" );
